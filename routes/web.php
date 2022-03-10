93949a3e9fedb64926b72e785fc4aaa28181be9a
Alienz <71365796+Annie-tmvs@users.noreply.github.com>
2022-03-10 21:09:37 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Frontend\CartController;
+use App\Http\Controllers\Frontend\CheckoutController;
+use App\Http\Controllers\Frontend\FrontendController;
+use App\Http\Controllers\Frontend\UserController;
+use Illuminate\Routing\Route as RoutingRoute;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('contact',[FrontendController::class,'contact']);
+Route::get('/', [FrontendController::class, 'index']);
+Route::get('category', [FrontendController::class, 'category']);
+Route::get('view-category/{slug}', [FrontendController::class, 'showcategory']);
+Route::get('category/{cate_slug}/{prod_slug}', [FrontendController::class,'showproduct']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('addtocart', [CartController::class, 'addProduct']);
+Route::post('delete-item', [CartController::class, 'deleteProduct']);
+Route::post('update-item', [CartController::class, 'updatecart']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('cart', [CartController::class, 'showcart']);
+    Route::get('checkout', [CheckoutController::class, 'index']);
+    Route::post('place-order', [CheckoutController::class, 'placeorder']);
+
+    Route::get('my-orders',[UserController::class, 'index']);
+    Route::get('view-order/{id}',[UserController::class, 'view']);
+});
+
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/dashboard', 'Admin\FrontendController@index');
+
+    Route::get('categories', 'Admin\CategoryController@index');
+    Route::get('add-categories', 'Admin\CategoryController@add');
+    Route::post('insert-category', 'Admin\CategoryController@insert');
+    Route::get('edit-category/{id}', [CategoryController::class, 'edit']);
+    Route::put('update-category/{id}', [CategoryController::class, 'update']);
+    Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
+
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('add-products', [ProductController::class, 'add']);
+    Route::post('insert-product', [ProductController::class, 'insert']);
+
+    Route::get('edit-product/{id}', [ProductController::class, 'edit']);
+    Route::put('update-product/{id}', [ProductController::class, 'update']);
+    Route::get('delete-product/{id}', [ProductController::class, 'destroy']);
+});
